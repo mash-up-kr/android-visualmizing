bf084bfa9baa8a81978d606dee74e4a4ee32b37a
DongHee Kim <opklnm102@gmail.com>
2015-12-27 03:48:51 +0900

    #1 메인 화면 구현

--- app/src/main/java/org/ewhappcenter/visualmizing/MainActivity.java
+++ app/src/main/java/org/ewhappcenter/visualmizing/MainActivity.java
@@ -1,8 +1,16 @@
 package org.ewhappcenter.visualmizing;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,33 +21,104 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.ewhappcenter.visualmizing.dummy.DummyContent;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, OnListFragmentInteractionListener {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @Bind(R.id.fab)
+    FloatingActionButton fabDV;
+
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drawer;
+
+    @Bind(R.id.nav_view)
+    NavigationView navigationView;
+    @Bind(R.id.tabs)
+    TabLayout mTabLayout;
+    @Bind(R.id.viewPager)
+    ViewPager mViewPager;
+    MainPagerAdapter mMainPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        setSupportActionBar(mToolbar);
+
+        final ActionBar ab = getSupportActionBar();
+        //ab.setHomeAsUpIndicator(R.mipmap.ic_launcher);  //set a custom icon for the default home button
+        ab.setDisplayShowHomeEnabled(true);  //show or hide the default home button
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setDisplayShowCustomEnabled(true);  //enable overriding the default toolbar layout
+        ab.setDisplayShowTitleEnabled(false);  //disable the default title element here(for centered title)
+
+        fabDV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, DataVisualizationActivity.class);
+                startActivity(intent);
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        setUpViewPager(mViewPager);
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private void setUpViewPager(ViewPager viewPager){
+        mMainPagerAdapter = new MainPagerAdapter(getSupportFragmentManager());
+        mMainPagerAdapter.addFragment(new TodayDataVisualizationFragment(), "오늘의 DV");
+        mMainPagerAdapter.addFragment(new MyDataVisualizationFragment(), "나의 DV");
+        mMainPagerAdapter.addFragment(new NewestDataVisualizationFragment(), "최신 DV");
+        viewPager.setAdapter(mMainPagerAdapter);
+    }
+
+    static class MainPagerAdapter extends FragmentPagerAdapter{
+        private final ArrayList<Fragment> mFragments = new ArrayList<>();
+        private final ArrayList<String> mFragmentTitles = new ArrayList<>();
+
+        public MainPagerAdapter(FragmentManager fm){
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title){
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position){
+            return mFragmentTitles.get(position);
+        }
     }
 
     @Override
@@ -97,5 +176,10 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        Log.e(TAG, "onListFragmentInteraction");
     }
 }
